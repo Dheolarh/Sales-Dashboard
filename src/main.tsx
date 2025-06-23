dbd449e0378c47db650c0f2ca07bd0c83862eaed
Adeola Adejuwonlo <adejuwonlookanlawon@gmail.com>
2025-06-23 18:58:50 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { AuthProvider } from './hooks/AuthContext' // Import AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider> {/* Wrap App with AuthProvider */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
